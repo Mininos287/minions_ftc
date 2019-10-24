ddaecc43d6ccddd753b0d3f07c198e63bc140f74
minions287 <khaledmostafa297@gmail.com>
2019-10-24 05:12:24 -0700

    add a method move_with_encoder

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HelperClass.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HelperClass.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class HelperClass {
+
+
+
+
 
 
     /**
@@ -63,8 +68,111 @@ public class HelperClass {
 
         }
 
-
     }
 
 
+
+
+
+
+    /**
+     * METHOD: dc_motor_power_adapter
+     *
+     * Used to convert from -100 to 100 (power) to -1 and 1
+     *
+     * parameter: double power
+     * return double
+     */
+    private double dc_motor_power_adapter(double power) {
+        double new_power = (power / 100);
+        return new_power;
+    }
+
+
+
+
+
+
+
+
+    /**
+     * METHOD: set_left_side_power
+     *
+     * Provide the two wheels on the left side with the same power
+     *
+     * parameters: double power
+     * return double
+     */
+    private void set_left_side_power(DcMotor left_back_wheel, DcMotor left_front_wheel, double power){
+        left_back_wheel.setPower(dc_motor_power_adapter(power));
+        left_front_wheel.setPower(dc_motor_power_adapter(power));
+    }
+
+
+
+
+
+
+    /**
+     * METHOD: set_right_side_power
+     *
+     * Provide the two wheels on the right side with the same power
+     *
+     * parameters: right_back_wheel,right_front_wheel, double power
+     *
+     * return double
+     */
+    private void set_right_side_power(DcMotor right_back_wheel, DcMotor right_front_wheel, double power){
+        right_back_wheel.setPower(dc_motor_power_adapter(power));
+        right_front_wheel.setPower(dc_motor_power_adapter(power));
+    }
+
+
+
+
+
+
+    /**
+     * METHOD: move_tank_without_encoder
+     *
+     * Used to give the same power to the right side and the same power to the left side
+     * parameters: power
+     *
+     * return double
+     */
+    public void move_tank_without_encoder(DcMotor left_back_wheel, DcMotor left_front_wheel,
+                                          DcMotor right_back_wheel, DcMotor right_front_wheel,
+                                          double left_side_power, double right_side_power){
+
+        set_left_side_power(left_back_wheel, left_front_wheel, left_side_power);
+
+        set_left_side_power(right_back_wheel, right_front_wheel, right_side_power);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
